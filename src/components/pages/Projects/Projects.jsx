3fcd067ad52eba75b4df360dc28ca054c5385c52
Abraham E. Tavarez <abetavarez@icloud.com>
2020-06-19 23:10:46 -0400

    Added project card to projects page and links to sites and repos

--- src/components/pages/Projects/Projects.jsx
+++ src/components/pages/Projects/Projects.jsx
@@ -31,36 +31,46 @@ export default function Projects() {
             <CardActionArea>
               <CardMedia
                 className={classes.media}
-                image="/static/images/cards/contemplative-reptile.jpg"
-                title="Contemplative Reptile"
+                image="https://res.cloudinary.com/abetavarez/image/upload/v1592620250/Screen_Shot_2020-06-19_at_10.30.27_PM_wofq1e.png"
+                title="stayhomecookhome-website"
               />
               <CardContent>
                 <Typography gutterBottom variant="h5" component="h2">
                   #StayHomeCookHome
                 </Typography>
                 <Typography variant="body2" color="textSecondary" component="p">
-                  Lizards are a widespread group of squamate reptiles, with over
-                  6,000 species, ranging across all continents except Antarctica
+                  Helping people from around the world to find quick easy and
+                  healthy recepies to cook at home and stay safe from the
+                  #covid19 virus.
                 </Typography>
               </CardContent>
             </CardActionArea>
             <CardActions>
-              <Button size="small" color="primary">
-                Share
+              <Button
+                size="small"
+                color="primary"
+                href="https://pages.git.generalassemb.ly/AbrahamEfrenTavarez/StayHomeCookHome/"
+              >
+                Visit Site
               </Button>
-              <Button size="small" color="primary">
-                Learn More
+              <Button
+                size="small"
+                color="primary"
+                href="https://github.com/AbeTavarez/-StayHomeCookHome"
+              >
+                Visit Repo
               </Button>
             </CardActions>
           </Card>
         </div>
+
         <div className="card-section">
           <Card className={classes.root}>
             <CardActionArea>
               <CardMedia
                 className={classes.media}
-                image="/static/images/cards/contemplative-reptile.jpg"
-                title="Contemplative Reptile"
+                image="https://res.cloudinary.com/abetavarez/image/upload/v1592621658/Screen_Shot_2020-06-19_at_10.52.40_PM_wzvrar.png"
+                title="streamnetfy-website"
               />
               <CardContent>
                 <Typography gutterBottom variant="h5" component="h2">
@@ -73,11 +83,19 @@ export default function Projects() {
               </CardContent>
             </CardActionArea>
             <CardActions>
-              <Button size="small" color="primary">
-                Share
+              <Button
+                size="small"
+                color="primary"
+                href="https://streamnetfy.netlify.app/"
+              >
+                Visit Site
               </Button>
-              <Button size="small" color="primary">
-                Learn More
+              <Button
+                size="small"
+                color="primary"
+                href="https://github.com/AbeTavarez/Streamnetfy"
+              >
+                Visit Repo
               </Button>
             </CardActions>
           </Card>
@@ -88,12 +106,40 @@ export default function Projects() {
             <CardActionArea>
               <CardMedia
                 className={classes.media}
+                image="https://res.cloudinary.com/abetavarez/image/upload/v1592622098/Screen_Shot_2020-06-19_at_11.01.22_PM_nds2kr.png"
+                title="command-blog-website"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  CommandBlog
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  Lizards are a widespread group of squamate reptiles, with over
+                  6,000 species, ranging across all continents except Antarctica
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <Button size="small" color="primary">
+                Share
+              </Button>
+              <Button size="small" color="primary">
+                Learn More
+              </Button>
+            </CardActions>
+          </Card>
+        </div>
+        <div className="card-section">
+          <Card className={classes.root}>
+            <CardActionArea>
+              <CardMedia
+                className={classes.media}
                 image="/static/images/cards/contemplative-reptile.jpg"
                 title="Contemplative Reptile"
               />
               <CardContent>
                 <Typography gutterBottom variant="h5" component="h2">
-                  CommandBlog
+                  InstaCart
                 </Typography>
                 <Typography variant="body2" color="textSecondary" component="p">
                   Lizards are a widespread group of squamate reptiles, with over
